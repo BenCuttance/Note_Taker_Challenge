d910f5cea72e13021db56b86b66b10fb35bd3b77
BenC <bcuttance1998@gmail.com>
2023-06-01 14:59:27 +0930

    added starter code for creating app.delete

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
-    console.log('beep boop')
     
+
 }
 );
 
 app.get('/notes', (req, res) => {
-    console.info(`${req.method} request for notes beep boop`);
+  
     res.sendFile(path.join(__dirname, 'public/notes.html'))
-    console.log('beep boop')
    
+
 }
 )
 
@@ -35,14 +35,15 @@ app.get('/api/notes', (req, res) => {
     console.log(`${req.method} request recieved for notes`);
     var notes = JSON.parse(readFromFile('./db/db.json'));
     // .then((data => res.json(JSON.parse(data))));
-        console.log(notes)
-        res.json(notes)
+    
+    res.json(notes)
 
 })
 
-const getId = () =>{ return Math.floor((1 + Math.random()) * 0x10000)
-.toString(16)
-.substring(1);
+const getId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
 }
 // POST request 
 
@@ -58,88 +59,53 @@ app.post('/api/notes', (req, res) => {
             id: getId()
         }
 
-    fs.readFile('./db/db.json', 'utf8', (err, data)=> {
-        if (err) {
-            console.error(err);
-        
-        } else {
-            const parsedNote = JSON.parse(data);
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
 
-            parsedNote.push(newNote);
+            } else {
+                const parsedNote = JSON.parse(data);
 
-            fs.writeFile(
-                './db/db.json',
-                JSON.stringify(parsedNote, null, 4),
-                (writeErr) => 
-                    writeErr
-                    ? console.error(writeErr)
-                    : res.send('New note recieved')
+                parsedNote.push(newNote);
 
-            )
-        }
-    })
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNote, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : res.send('New note recieved')
+
+                )
+            }
+        })
     }
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    console.log('delete request called')
+    const id = req.params.id 
+    console.log(id)
+    
+    var note = note.id(parseInt(req.params.id));
+
+    if (note) {
+        removeNote(parseInt(req.params.id))
 
 
+    return res.send('Note deleted')
+    } 
 
-// app.post('/api/notes', (req, res) => {
+})  
 
-//     console.info(`${req.method} request received to add a review`);
 
-//     const { title, text } = req.body;
-
-//     if (req.body) {
-//         const newNote = {
-//             title,
-//             text
-//         };
-
-//         readAndAppend(newNote, './db/db.json');
-//         res.json('Note added successfully') 
-   
-//         // Obtain existing notes
-//         fs.readFile('./db/db.json', 'utf8', (err, data) => {
-//             if (err) {
-//                 console.error(err);
-//             } else {
-//                 const parsedNote = json.parse(data);
-
-//                 parsedNote.push(newNote);
-
-//                 fs.writeFile(
-//                     './db/db.json',
-//                     JSON.stringify(parsedNote, null, 4),
-//                     (writeErr) =>
-//                         writeErr
-//                             ? console.error(writeErr)
-//                             : console.info('Successfully updated notes')
-//                 )
-//             }
-
-//         });
-
-//         const response = {
-//             status: 'success',
-//             body: newNote,
-//         };
-
-//         console.log(response);
-//         res.status(201).json(response);
-//     } else {
-//         res.status(500).json('Error in posting note')
-//     }
-
-// }
-// )
 
 app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
 
 // Fall back pathway 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
- 
+
 }
 );
 
