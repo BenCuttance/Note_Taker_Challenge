b48d92a163be1c84d1f24742e9ee3b9852356b37
BenC <bcuttance1998@gmail.com>
2023-06-01 15:58:28 +0930

    finished updated app.delete and also added comment for future coders

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+
 const path = require('path')
 const express = require('express');
 const fs = require('fs')
-// const api = require('.'); 
 
+// Local port and port for use on Heroku
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.static('public'));
 
-
+// Code to get route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
     
-
 }
 );
 
+// Code to get route for the notes page
 app.get('/notes', (req, res) => {
   
     res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -24,29 +26,31 @@ app.get('/notes', (req, res) => {
 
 }
 )
-
+// Function to read file data
 const readFromFile = (path) => {
     const fileData = fs.readFileSync(path);
     return fileData;
 
 }
 
+// Code to read data from db.json and display data onto users page
 app.get('/api/notes', (req, res) => {
     console.log(`${req.method} request recieved for notes`);
     var notes = JSON.parse(readFromFile('./db/db.json'));
-    // .then((data => res.json(JSON.parse(data))));
+   
     
     res.json(notes)
 
 })
 
+// Function to randomly generate unique Id for each note generated for the database
 const getId = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
         .substring(1);
 }
-// POST request 
 
+// POST request 
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} for notes`);
 
@@ -82,24 +86,34 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
+// Code to delete notes from database. This code return all notes in databse, except the note with the ID the user selected
+// Then the notes are re-written back to the databse.
 app.delete('/api/notes/:id', (req, res) => {
     console.log('delete request called')
     const id = req.params.id 
     console.log(id)
     
-    var note = note.id(parseInt(req.params.id));
+    var notes = JSON.parse(readFromFile('./db/db.json')); 
+    
+    var updatedNotes = notes.filter((note) => {
+        return note.id !== id;
+    }) ;
 
-    if (note) {
-        removeNote(parseInt(req.params.id))
+    console.log(updatedNotes);
+    
+    fs.writeFile(
+        './db/db.json',
+        JSON.stringify(updatedNotes, null, 4),
+        (writeErr) =>
+            writeErr
+                ? console.error(writeErr)
+                : res.send('Notes updated') 
+)} 
 
+ 
+)  
 
-    return res.send('Note deleted')
-    } 
-
-})  
-
-
-
+// Starting the server
 app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
 
 // Fall back pathway 
